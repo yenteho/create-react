dec25f714aacbc4c39393955f16bbe1b64b83f4a
Angel <angel@twdt260.ict888.net>
2018-11-06 18:14:55 +0800

    Functional Components to Class Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyARZw7GnidoyFcBPlMIFxMyhIuHPu7jxic';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return <div>
-    < SearchBar />;
-  </div>;
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term:'Ariana Grande'}, (videos) => {
+      this.setState({ videos });
+    });
+  } 
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it on the page( in the DOM)
